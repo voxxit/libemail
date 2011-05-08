714a1bb559188878bc2164cd54c5225443965be7
Peter Blair <petermblair@gmail.com>
2011-05-07 23:17:47 -0400

    Testing parts

--- cpp/cidr/main.cpp
+++ cpp/cidr/main.cpp
@@ -1,15 +1,37 @@
 #include "cidr.hpp"
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 int main(void){
   try{
-    CIDR c( "192.168.3.0/24" );
-   
-    if( c.in( IP("192.168.3.5") ) ){
-      std::cout << "IP is in CIDR" << std::endl;
-    }
-    else {
-      std::cerr << "IP not found in CIDR" << std::endl;
+
+    std::vector< CIDR > whitelist;
+
+    whitelist.push_back( CIDR ( "192.168.3.0/24" ) );
+    whitelist.push_back( CIDR ( "192.168.6.0/24" ) );
+    whitelist.push_back( CIDR ( "192.168.9.0/24" ) );
+    whitelist.push_back( CIDR ( "192.168.22.0/24" ) );
+
+    while( std::cin ){
+  
+      std::string strIP;			  
+      std::cin >> strIP;
+
+      if( strIP.size() == 0 )
+	continue;
+
+      std::cout << "Processing " << strIP << std::endl;
+
+      // Create an iterator
+      std::vector< CIDR >::iterator i = whitelist.begin();
+
+      for( ; i != whitelist.end(); i++ ){
+	if( (*i).in( IP( strIP ) ) ){
+	  std::cout << "IP " << strIP << " is whitelisted" << std::endl;
+	  break;
+	}
+      }
     }
   }
   catch( std::string &e ){
